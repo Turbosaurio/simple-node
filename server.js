e8ddf0a759435ec91d1539d243632af05514349f
Reinhold <reinholandia@gmail.com>
2021-03-21 21:01:41 -0600

    add env var

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+require('dotenv').config()
+
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
+		console.log(`My favorite food is ${favoriteFood}`);
     console.log('Containers rule!');
     await sleep(5000);
   }
